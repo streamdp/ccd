881fc67daba45d09cadfd6194d799111669070f8
Alexandr Primak <primak.aa@gmail.com>
2021-11-27 05:03:39 +0300

    refactored code, transferred db.Connect to dbconnectors package in  function ServePipe() .

--- dbconnectors/mysqlconnector.go
+++ dbconnectors/mysqlconnector.go
@@ -12,7 +12,7 @@ type Db struct {
 	instance *sql.DB
 }
 
-func (d *Db) GetLast(from string, to string) (result  *dataproviders.Data, err error) {
+func (d *Db) GetLast(from string, to string) (result *dataproviders.Data, err error) {
 	r := &dataproviders.Response{}
 	result = dataproviders.GetEmptyData(from, to)
 	row := d.instance.QueryRow("select change24hour, changepct24hour, open24hour, volume24hour, low24hour, high24hour, price, supply, mktcap, lastupdate from cryptocompare.data where fromSym=(select _id from fsym where symbol=?) and toSym=(select _id from tsym where symbol=?) ORDER BY lastupdate DESC limit 1;", from, to)
@@ -26,8 +26,8 @@ func (d *Db) GetLast(from string, to string) (result  *dataproviders.Data, err e
 
 func (d *Db) Insert(data *dataproviders.DataPipe) (result sql.Result, err error) {
 	r := data.Data.Raw[data.From][data.To]
-	result, err = d.instance.Exec("insert into data (fromSym, toSym, change24hour, changepct24hour, open24hour, volume24hour, low24hour, high24hour, price, supply, mktcap, lastupdate) values ((SELECT _id FROM fsym WHERE symbol=?),(SELECT _id FROM tsym WHERE symbol=?),?,?,?,?,?,?,?,?,?,?)", data.From, data.To, r.Change24Hour,r.Changepct24Hour, r.Open24Hour, r.Volume24Hour, r.Low24Hour, r.High24Hour, r.Price, r.Supply, r.Mktcap, r.Lastupdate)
-	if err != nil{
+	result, err = d.instance.Exec("insert into data (fromSym, toSym, change24hour, changepct24hour, open24hour, volume24hour, low24hour, high24hour, price, supply, mktcap, lastupdate) values ((SELECT _id FROM fsym WHERE symbol=?),(SELECT _id FROM tsym WHERE symbol=?),?,?,?,?,?,?,?,?,?,?)", data.From, data.To, r.Change24Hour, r.Changepct24Hour, r.Open24Hour, r.Volume24Hour, r.Low24Hour, r.High24Hour, r.Price, r.Supply, r.Mktcap, r.Lastupdate)
+	if err != nil {
 		return nil, errors.Wrapf(err, "can't insert data")
 	}
 	return
@@ -44,7 +44,22 @@ func (d *Db) Close() (err error) {
 	return d.instance.Close()
 }
 
-func ServePipe(pipe chan *dataproviders.DataPipe, db *Db) {
+func ServePipe(pipe chan *dataproviders.DataPipe) {
+	var db Db
+	dataSource := utility.GetEnv("CCDC_DATASOURCE")
+	if dataSource == "" {
+		utility.HandleError(errors.New("please set OS environment \"CCDC_DATASOURCE\" with database connection string"))
+		return
+	}
+	if err := db.Connect(dataSource); err != nil {
+		utility.HandleError(err)
+		return
+	}
+	defer func(d *Db) {
+		if err := d.Close(); err != nil {
+			utility.HandleError(err)
+		}
+	}(&db)
 	for {
 		if data := <-pipe; true {
 			if _, err := db.Insert(data); err != nil {
--- main.go
+++ main.go
@@ -16,16 +16,10 @@ func main() {
 	var ccData *cryptocompare.CryptoCompare
 	var showHelp bool
 	var port int
-	var d dbconnectors.Db
 	ccData.SetApiKey(utility.GetEnv("CCDC_APIKEY"))
 	ccData.SetApiURL(utility.GetEnv("CCDC_APIURL"))
 	if ccData.GetApiURL() == "" || ccData.GetApiKey() == "" {
 		utility.HandleError(errors.New("you should specify \"CCDC_APIKEY\" and \"CCDC_APIURL\" in you OS environment"))
-		return
-	}
-	dataSource := utility.GetEnv("CCDC_DATASOURCE")
-	if dataSource == "" {
-		utility.HandleError(errors.New("please set OS environment \"CCDC_DATASOURCE\" with database connection string"))
 		return
 	}
 	flag.BoolVar(&showHelp, "h", false, "display help")
@@ -37,19 +31,10 @@ func main() {
 		flag.Usage()
 		return
 	}
-	if err := d.Connect(dataSource); err != nil {
-		utility.HandleError(err)
-		return
-	}
-	defer func(d *dbconnectors.Db) {
-		if err := d.Close(); err != nil {
-			utility.HandleError(err)
-		}
-	}(&d)
 	wc := dataproviders.CreateWorkersControl()
 	pipe := make(chan *dataproviders.DataPipe, 20)
 	defer close(pipe)
-	go dbconnectors.ServePipe(pipe, &d)
+	go dbconnectors.ServePipe(pipe)
 	r := gin.Default()
 	if err := r.SetTrustedProxies(nil); err != nil {
 		utility.HandleError(err)
@@ -58,7 +43,6 @@ func main() {
 	r.POST("/collect/add", collectAdd(ccData, wc, pipe))
 	r.POST("/collect/remove", collectRemove(wc))
 	r.GET("/collect/status", collectStatus(wc))
-
 	if err := r.Run(":" + strconv.Itoa(port)); err != nil {
 		return
 	}

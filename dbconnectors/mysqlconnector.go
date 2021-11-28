8343a3656584e4d4904bdc8cf42ea42231307493
Alexandr Primak <primak.aa@gmail.com>
2021-11-28 06:23:53 +0300

    Added "display" data as raw json from dataprovider, previosly formatted output simplyfy work with this data.

--- dbconnectors/mysqlconnector.go
+++ dbconnectors/mysqlconnector.go
@@ -2,68 +2,74 @@ package dbconnectors
 
 import (
 	"database/sql"
+	"encoding/json"
+	"errors"
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/pkg/errors"
 	"github.com/streamdp/ccdatacollector/dataproviders"
+	"github.com/streamdp/ccdatacollector/handlers"
 	"github.com/streamdp/ccdatacollector/utility"
+	"time"
 )
 
 type Db struct {
 	instance *sql.DB
 }
 
-func (d *Db) GetLast(from string, to string) (result *dataproviders.Data, err error) {
-	r := &dataproviders.Response{}
+func (db *Db) GetLast(from string, to string) (result *dataproviders.Data, err error) {
+	var displayDataRaw string
 	result = dataproviders.GetEmptyData(from, to)
-	row := d.instance.QueryRow("select change24hour, changepct24hour, open24hour, volume24hour, low24hour, high24hour, price, supply, mktcap, lastupdate from cryptocompare.data where fromSym=(select _id from fsym where symbol=?) and toSym=(select _id from tsym where symbol=?) ORDER BY lastupdate DESC limit 1;", from, to)
-	err = row.Scan(&r.Change24Hour, &r.Changepct24Hour, &r.Open24Hour, &r.Volume24Hour, &r.Low24Hour, &r.High24Hour, &r.Price, &r.Supply, &r.Mktcap, &r.Lastupdate)
-	result.Raw[from][to] = *r
+	r := result.Raw[from][to]
+	row := db.instance.QueryRow("select change24hour, changepct24hour, open24hour, volume24hour, low24hour, high24hour, price, supply, mktcap, lastupdate, displaydataraw from cryptocompare.data where fromSym=(select _id from fsym where symbol=?) and toSym=(select _id from tsym where symbol=?) ORDER BY lastupdate DESC limit 1;", from, to)
+	err = row.Scan(&r.Change24Hour, &r.Changepct24Hour, &r.Open24Hour, &r.Volume24Hour, &r.Low24Hour, &r.High24Hour, &r.Price, &r.Supply, &r.Mktcap, &r.Lastupdate, &displayDataRaw)
 	if err != nil {
-		return &dataproviders.Data{}, errors.Wrapf(err, "can't match data between row and dest")
+		return nil, err
 	}
+	_ = json.Unmarshal([]byte(displayDataRaw), &result.Display)
+	result.Display[from][to].Lastupdate = time.Unix(result.Raw[from][to].Lastupdate, 0).String()
 	return result, nil
 }
 
-func (d *Db) Insert(data *dataproviders.DataPipe) (result sql.Result, err error) {
+func (db *Db) Insert(data *dataproviders.DataPipe) (result sql.Result, err error) {
 	r := data.Data.Raw[data.From][data.To]
-	result, err = d.instance.Exec("insert into data (fromSym, toSym, change24hour, changepct24hour, open24hour, volume24hour, low24hour, high24hour, price, supply, mktcap, lastupdate) values ((SELECT _id FROM fsym WHERE symbol=?),(SELECT _id FROM tsym WHERE symbol=?),?,?,?,?,?,?,?,?,?,?)", data.From, data.To, r.Change24Hour, r.Changepct24Hour, r.Open24Hour, r.Volume24Hour, r.Low24Hour, r.High24Hour, r.Price, r.Supply, r.Mktcap, r.Lastupdate)
+	d, err := json.Marshal(data.Data.Display)
 	if err != nil {
-		return nil, errors.Wrapf(err, "can't insert data")
+		return nil, err
+	}
+	result, err = db.instance.Exec("insert into data (fromSym, toSym, change24hour, changepct24hour, open24hour, volume24hour, low24hour, high24hour, price, supply, mktcap, lastupdate, displaydataraw) values ((SELECT _id FROM fsym WHERE symbol=?),(SELECT _id FROM tsym WHERE symbol=?),?,?,?,?,?,?,?,?,?,?,?)", data.From, data.To, &r.Change24Hour, &r.Changepct24Hour, &r.Open24Hour, &r.Volume24Hour, &r.Low24Hour, &r.High24Hour, &r.Price, &r.Supply, &r.Mktcap, &r.Lastupdate, string(d))
+	if err != nil {
+		return nil, err
 	}
 	return
 }
 
-func (d *Db) Connect(dataSource string) (err error) {
-	if d.instance, err = sql.Open("mysql", dataSource); err != nil {
-		return errors.Wrapf(err, "can't connect to database")
+func (db *Db) Connect(dataSource string) (err error) {
+	if db.instance, err = sql.Open("mysql", dataSource); err != nil {
+		return err
 	}
 	return nil
 }
 
-func (d *Db) Close() (err error) {
-	return d.instance.Close()
+func (db *Db) Close() (err error) {
+	return db.instance.Close()
 }
 
-func ServePipe(pipe chan *dataproviders.DataPipe) {
-	var db Db
+func Connect() (db *Db, err error) {
+	db = &Db{}
 	dataSource := utility.GetEnv("CCDC_DATASOURCE")
 	if dataSource == "" {
-		utility.HandleError(errors.New("please set OS environment \"CCDC_DATASOURCE\" with database connection string"))
-		return
+		return nil, errors.New("please set OS environment \"CCDC_DATASOURCE\" with database connection string")
 	}
 	if err := db.Connect(dataSource); err != nil {
-		utility.HandleError(err)
-		return
+		return nil, err
 	}
-	defer func(d *Db) {
-		if err := d.Close(); err != nil {
-			utility.HandleError(err)
-		}
-	}(&db)
+	return db, nil
+}
+
+func (db *Db) ServePipe(pipe chan *dataproviders.DataPipe) {
 	for {
 		if data := <-pipe; true {
 			if _, err := db.Insert(data); err != nil {
-				utility.HandleError(err)
+				handlers.SystemHandler(err)
 			}
 		}
 	}

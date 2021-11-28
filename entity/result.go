3967a85b897b393443c9e404fd9e08eafb564749
Alexandr Primak <primak.aa@gmail.com>
2021-11-28 06:27:48 +0300

    Added a Result structure for use with an error and message handler. Simplified code, output looks better.

---
+++ entity/result.go
@@ -0,0 +1,29 @@
+package entity
+
+type Result struct {
+	Code    int         `json:"code"`
+	Message string      `json:"msg"`
+	Data    interface{} `json:"data"`
+}
+
+func (res *Result) SetCode(code int) *Result {
+	res.Code = code
+	return res
+}
+
+func (res *Result) UpdateAllFields(code int, msg string, data interface{}) *Result {
+	res.SetCode(code)
+	res.SetMessage(msg)
+	res.SetData(data)
+	return res
+}
+
+func (res *Result) SetMessage(msg string) *Result {
+	res.Message = msg
+	return res
+}
+
+func (res *Result) SetData(data interface{}) *Result {
+	res.Data = data
+	return res
+}

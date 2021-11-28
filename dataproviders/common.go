446b6dd90ca1c83f2e617af9df155db2458cbb4c
Alexandr Primak <primak.aa@gmail.com>
2021-11-28 06:43:41 +0300

    Added Display struct, extended Data struct with Display and updated all needed methods.

--- dataproviders/common.go
+++ dataproviders/common.go
@@ -1,80 +1,77 @@
 package dataproviders
 
 import (
-	"encoding/json"
-	"github.com/pkg/errors"
-	"io/ioutil"
-	"net/http"
-	"net/url"
 	"time"
 )
 
 type Response struct {
-		Change24Hour    float64 `json:"CHANGE24HOUR"`
-		Changepct24Hour float64 `json:"CHANGEPCT24HOUR"`
-		Open24Hour      float64 `json:"OPEN24HOUR"`
-		Volume24Hour    float64 `json:"VOLUME24HOUR"`
-		Volume24Hourto  float64 `json:"VOLUME24HOURTO"`
-		Low24Hour       float64 `json:"LOW24HOUR"`
-		High24Hour      float64 `json:"HIGH24HOUR"`
-		Price           float64 `json:"PRICE"`
-		Supply          float64 `json:"SUPPLY"`
-		Mktcap          float64 `json:"MKTCAP"`
-		Lastupdate      int     `json:"LASTUPDATE"`
+	Change24Hour    float64 `json:"CHANGE24HOUR"`
+	Changepct24Hour float64 `json:"CHANGEPCT24HOUR"`
+	Open24Hour      float64 `json:"OPEN24HOUR"`
+	Volume24Hour    float64 `json:"VOLUME24HOUR"`
+	Volume24Hourto  float64 `json:"VOLUME24HOURTO"`
+	Low24Hour       float64 `json:"LOW24HOUR"`
+	High24Hour      float64 `json:"HIGH24HOUR"`
+	Price           float64 `json:"PRICE"`
+	Supply          float64 `json:"SUPPLY"`
+	Mktcap          float64 `json:"MKTCAP"`
+	Lastupdate      int64   `json:"LASTUPDATE"`
+}
+
+type Display struct {
+	Change24Hour    string `json:"CHANGE24HOUR"`
+	Changepct24Hour string `json:"CHANGEPCT24HOUR"`
+	Open24Hour      string `json:"OPEN24HOUR"`
+	Volume24Hour    string `json:"VOLUME24HOUR"`
+	Volume24Hourto  string `json:"VOLUME24HOURTO"`
+	High24Hour      string `json:"HIGH24HOUR"`
+	Price           string `json:"PRICE"`
+	FromSymbol      string `json:"FROMSYMBOL"`
+	ToSymbol        string `json:"TOSYMBOL"`
+	Lastupdate      string `json:"LASTUPDATE"`
+	Supply          string `json:"SUPPLY"`
+	Mktcap          string `json:"MKTCAP"`
 }
 
 type Data struct {
-	Raw map[string]map[string]Response `json:"RAW"`
+	Raw     map[string]map[string]*Response `json:"RAW"`
+	Display map[string]map[string]*Display  `json:"DISPLAY"`
+}
+
+type DisplayData struct {
 }
 
 type DataPipe struct {
 	From string
-	To string
+	To   string
 	Data *Data
 }
 
 type DataProvider interface {
 	GetSerializable() *Data
-	GetApiURLToCompare(fSym string, tSym string) (*url.URL, error)
+	GetData(from string, to string) (*Data, error)
 }
 
-func PullingData(dp DataProvider, from string, to string, interval int, worker *Worker) (err error){
-	var (
-		data     []byte
-		response *http.Response
-		apiURL   *url.URL
-	)
+func PullingData(wc *Workers, from string, to string) (err error) {
+	worker := wc.GetWorker(from, to)
 	defer func(w *Worker) {
 		w.SetAlive(false)
 		close(w.GetDone())
 	}(worker)
 	worker.SetAlive(true)
-	if apiURL, err = dp.GetApiURLToCompare(from, to); err != nil {
-		return errors.Wrapf(err, "failed to get url")
-	}
 	for {
 		select {
 		case <-worker.GetDone():
 			return
-		case <-time.After(time.Duration(interval) * time.Second):
-			if response, err = http.Get(apiURL.String()); err != nil {
-				return errors.Wrapf(err, "failed to open URL")
+		case <-time.After(time.Duration(worker.Interval) * time.Second):
+			data, err := wc.Dp.GetData(from, to)
+			if err != nil {
+				return err
 			}
-			if data, err = ioutil.ReadAll(response.Body); err != nil {
-				return errors.Wrapf(err,"failed to read response body")
-			}
-			if response.StatusCode == 200 {
-				ds := dp.GetSerializable()
-				if err = json.Unmarshal(data, &ds); err != nil {
-					return errors.Wrapf(err, "failed to unmarshal data")
-				}
-				worker.GetPipe() <- &DataPipe{
-					From:from,
-					To: to,
-					Data: ds,
-				}
-			} else {
-				return errors.Wrapf(errors.New("server error:"), "error getting data with %v statuscode", response.StatusCode)
+			worker.GetPipe() <- &DataPipe{
+				From: from,
+				To:   to,
+				Data: data,
 			}
 		}
 	}
@@ -82,9 +79,12 @@ func PullingData(dp DataProvider, from string, to string, interval int, worker *
 
 func GetEmptyData(from string, to string) *Data {
 	result := &Data{
-		Raw: map[string]map[string]Response{},
+		Raw:     map[string]map[string]*Response{},
+		Display: map[string]map[string]*Display{},
 	}
-	result.Raw[from] = make(map[string]Response)
-	result.Raw[from][to] = Response{}
+	result.Raw[from] = make(map[string]*Response)
+	result.Raw[from][to] = &Response{}
+	result.Display[from] = make(map[string]*Display)
+	result.Display[from][to] = &Display{}
 	return result
 }

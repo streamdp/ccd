ac6ba6de31cd1258280c1689ec136865e68b4ee3
Alexandr Primak <primak.aa@gmail.com>
2021-12-02 20:04:17 +0300

    Added display of the status of running data collection nodes

--- site/js/site.js
+++ site/js/site.js
@@ -1,3 +1,5 @@
+get_list_with_active_nodes();
+window.setInterval(get_list_with_active_nodes, 5000);
 const mq = window.matchMedia('(prefers-color-scheme: dark)')
 if (mq.matches) {
     document.documentElement.classList.add('dark')
@@ -9,4 +11,33 @@ function toggleTheme() {
         const isDark = root.classList.contains('dark')
         window.ccd.changeTheme(isDark ? 'dark' : 'light')
     }
+}
+
+function get_list_with_active_nodes() {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open('GET', "v1/collect/status", true);
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState === 4) {
+            if(xmlhttp.status === 200) {
+                let obj = JSON.parse(xmlhttp.responseText);
+                let data = obj.data;
+                if (data === null) {
+                    document.getElementById("running-nodes").innerHTML = "Data is not currently being collected";
+                    return
+                }
+                let result = "<strong>Data is currently being collected for:</strong><ul>";
+                for (let fsym in data){
+                    let nodes = data[fsym];
+                    let litag = "<li>" + fsym + " to: "
+                    for (let tsym in nodes){
+                         litag += tsym+ " (interval: " + nodes[tsym]["interval"] + "), ";
+                    }
+                    result += litag.slice(0, -2) + "</li>";
+                }
+                result += "</ul>"
+                document.getElementById("running-nodes").innerHTML = result
+            }
+        }
+    };
+    xmlhttp.send(null);
 }

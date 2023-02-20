19e05ead3f2914b5a6bf29b558456e7e6ef5b917
Alexandr Primak <alexandr.primak@bequant.io>
2023-02-21 02:25:03 +0300

    refactored mysql client

--- dbconnectors/mysqlconnector.go
+++ dbconnectors/mysqlconnector.go
@@ -14,7 +14,7 @@ import (
 
 // Db needed to add new methods for an instance *sql.Db
 type Db struct {
-	instance *sql.DB
+	*sql.DB
 }
 
 // GetLast row with the most recent data for the selected currencies pair
@@ -22,7 +22,7 @@ func (db *Db) GetLast(from string, to string) (result *dataproviders.Data, err e
 	var displayDataRaw string
 	result = dataproviders.GetEmptyData(from, to)
 	r := result.Raw[from][to]
-	row := db.instance.QueryRow("select change24hour, changepct24hour, open24hour, volume24hour, low24hour, high24hour, price, supply, mktcap, lastupdate, displaydataraw from cryptocompare.data where fromSym=(select _id from fsym where symbol=?) and toSym=(select _id from tsym where symbol=?) ORDER BY lastupdate DESC limit 1;", from, to)
+	row := db.QueryRow("select change24hour, changepct24hour, open24hour, volume24hour, low24hour, high24hour, price, supply, mktcap, lastupdate, displaydataraw from cryptocompare.data where fromSym=(select _id from fsym where symbol=?) and toSym=(select _id from tsym where symbol=?) ORDER BY lastupdate DESC limit 1;", from, to)
 	err = row.Scan(&r.Change24Hour, &r.Changepct24Hour, &r.Open24Hour, &r.Volume24Hour, &r.Low24Hour, &r.High24Hour, &r.Price, &r.Supply, &r.Mktcap, &r.Lastupdate, &displayDataRaw)
 	if err != nil {
 		return nil, err
@@ -42,36 +42,29 @@ func (db *Db) Insert(data *dataproviders.DataPipe) (result sql.Result, err error
 	if err != nil {
 		return nil, err
 	}
-	result, err = db.instance.Exec("insert into data (fromSym, toSym, change24hour, changepct24hour, open24hour, volume24hour, low24hour, high24hour, price, supply, mktcap, lastupdate, displaydataraw) values ((SELECT _id FROM fsym WHERE symbol=?),(SELECT _id FROM tsym WHERE symbol=?),?,?,?,?,?,?,?,?,?,?,?)", data.From, data.To, &r.Change24Hour, &r.Changepct24Hour, &r.Open24Hour, &r.Volume24Hour, &r.Low24Hour, &r.High24Hour, &r.Price, &r.Supply, &r.Mktcap, &r.Lastupdate, string(d))
+	result, err = db.Exec("insert into data (fromSym, toSym, change24hour, changepct24hour, open24hour, volume24hour, low24hour, high24hour, price, supply, mktcap, lastupdate, displaydataraw) values ((SELECT _id FROM fsym WHERE symbol=?),(SELECT _id FROM tsym WHERE symbol=?),?,?,?,?,?,?,?,?,?,?,?)", data.From, data.To, &r.Change24Hour, &r.Changepct24Hour, &r.Open24Hour, &r.Volume24Hour, &r.Low24Hour, &r.High24Hour, &r.Price, &r.Supply, &r.Mktcap, &r.Lastupdate, string(d))
 	if err != nil {
 		return nil, err
 	}
 	return
 }
 
-func (db *Db) connect(dataSource string) (err error) {
-	if db.instance, err = sql.Open("mysql", dataSource); err != nil {
-		return err
-	}
-	return nil
-}
-
 // Close Db connection
 func (db *Db) Close() (err error) {
-	return db.instance.Close()
+	return db.Close()
 }
 
 // Connect after prepare to the Db
 func Connect() (db *Db, err error) {
-	db = &Db{}
 	dataSource := config.GetEnv("CCDC_DATASOURCE")
 	if dataSource == "" {
 		return nil, errors.New("please set OS environment \"CCDC_DATASOURCE\" with database connection string")
 	}
-	if err = db.connect(dataSource); err != nil {
-		return nil, err
+	sqlDb := &sql.DB{}
+	if sqlDb, err = sql.Open("mysql", dataSource); err != nil {
+		return
 	}
-	return db, nil
+	return &Db{sqlDb}, nil
 }
 
 // ServePipe and if we get dataproviders.DataPipe then save them to the Db

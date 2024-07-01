58eb32312d086f8b470d5950eba007512d0a72b3
Alexandr Primak <primak.aa@gmail.com>
2024-07-01 14:30:40 +0300

    covered huobi ws client with unit tests, small fixes.

--- clients/huobi/ws_client_test.go
+++ clients/huobi/ws_client_test.go
@@ -1,44 +1,11 @@
-package cryptocompare
+package huobi
 
 import (
-	"net/url"
 	"reflect"
 	"testing"
 
 	"github.com/streamdp/ccd/domain"
 )
-
-func Test_cryptoCompareWs_buildURL(t *testing.T) {
-	tests := []struct {
-		name    string
-		apiKey  string
-		wantU   *url.URL
-		wantErr bool
-	}{
-		{
-			name:   "build url",
-			apiKey: "4pwP6HmdD0O8PGDAkvKA9DSCGK74Ixma",
-			wantU: func() *url.URL {
-				u, _ := url.Parse("wss://streamer.cryptocompare.com/v2?api_key=4pwP6HmdD0O8PGDAkvKA9DSCGK74Ixma")
-				return u
-			}(),
-			wantErr: false,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			c := &cryptoCompareWs{apiKey: tt.apiKey}
-			gotU, err := c.buildURL()
-			if (err != nil) != tt.wantErr {
-				t.Errorf("buildURL() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(gotU, tt.wantU) {
-				t.Errorf("buildURL() gotU = %v, want %v", gotU, tt.wantU)
-			}
-		})
-	}
-}
 
 func Test_buildChannelName(t *testing.T) {
 	type args struct {
@@ -53,24 +20,65 @@ func Test_buildChannelName(t *testing.T) {
 		{
 			name: "build channel name",
 			args: args{
-				from: "BTC",
-				to:   "ETH",
-			},
-			want: "5~CCCAGG~BTC~ETH",
-		},
-		{
-			name: "build from lower",
-			args: args{
 				from: "btc",
 				to:   "eth",
 			},
-			want: "5~CCCAGG~BTC~ETH",
+			want: "market.btceth.ticker",
+		},
+		{
+			name: "usd case",
+			args: args{
+				from: "btc",
+				to:   "usd",
+			},
+			want: "market.btcusdt.ticker",
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := buildChannelName(tt.args.from, tt.args.to); got != tt.want {
 				t.Errorf("buildChannelName() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_huobiWs_pairFromChannelName(t *testing.T) {
+	tests := []struct {
+		name          string
+		subscriptions domain.Subscriptions
+		ch            string
+		wantFrom      string
+		wantTo        string
+	}{
+		{
+			name: "get pair from channel name",
+			subscriptions: map[string]*domain.Subscription{
+				buildChannelName("btc", "usdt"): domain.NewSubscription("btc", "usdt", 0),
+			},
+			ch:       buildChannelName("btc", "usdt"),
+			wantFrom: "BTC",
+			wantTo:   "USDT",
+		},
+		{
+			name:          "unknown channel name",
+			subscriptions: map[string]*domain.Subscription{},
+			ch:            buildChannelName("btc", "usdt"),
+			wantFrom:      "",
+			wantTo:        "",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			h := &huobiWs{
+				subscriptions: tt.subscriptions,
+			}
+			gotFrom, gotTo := h.pairFromChannelName(tt.ch)
+			if gotFrom != tt.wantFrom {
+				t.Errorf("pairFromChannelName() gotFrom = %v, want %v", gotFrom, tt.wantFrom)
+			}
+			if gotTo != tt.wantTo {
+				t.Errorf("pairFromChannelName() gotTo = %v, want %v", gotTo, tt.wantTo)
 			}
 		})
 	}
@@ -99,7 +107,7 @@ func Test_cryptoCompareWs_ListSubscriptions(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			c := &cryptoCompareWs{
+			c := &huobiWs{
 				subscriptions: tt.subscriptions,
 			}
 			if got := c.ListSubscriptions(); !reflect.DeepEqual(got, tt.want) {
@@ -109,47 +117,57 @@ func Test_cryptoCompareWs_ListSubscriptions(t *testing.T) {
 	}
 }
 
-func Test_convertCryptoCompareWsDataToDomain(t *testing.T) {
+func Test_convertHuobiWsDataToDomain(t *testing.T) {
+	type args struct {
+		from string
+		to   string
+		d    *huobiWsData
+	}
 	tests := []struct {
 		name string
-		d    *cryptoCompareWsData
+		args args
 		want *domain.Data
 	}{
 		{
 			name: "regular conversion",
-			d: &cryptoCompareWsData{
-				FromSymbol:          "BTC",
-				ToSymbol:            "ETH",
-				Price:               61391.27,
-				LastUpdate:          1719731197640,
-				Volume24Hour:        666.1991214442407,
-				Volume24HourTo:      40641405.87766658,
-				Open24Hour:          60867.47,
-				High24Hour:          61403.89,
-				Low24Hour:           60867.47,
-				CurrentSupply:       30701,
-				CurrentSupplyMktCap: 1215020604533,
+			args: args{
+				from: "btc",
+				to:   "usdt",
+				d: &huobiWsData{
+					Ch: "market.btcusdt.ticker",
+					Ts: 1719731197640,
+					Tick: wsTick{
+						Open:   60867.47,
+						High:   61403.89,
+						Low:    60867.47,
+						Amount: 666.1991214442407,
+						Vol:    40641405.87766658,
+						Count:  30701,
+						Bid:    61391.27,
+					},
+				},
 			},
 			want: &domain.Data{
-				FromSymbol:   "BTC",
-				ToSymbol:     "ETH",
+				FromSymbol:   "btc",
+				ToSymbol:     "usdt",
 				Open24Hour:   60867.47,
 				Volume24Hour: 666.1991214442407,
 				Low24Hour:    60867.47,
 				High24Hour:   61403.89,
 				Price:        61391.27,
 				Supply:       30701,
-				MktCap:       1215020604533,
 				LastUpdate:   1719731197640,
-				DisplayDataRaw: "{\"from_symbol\":\"BTC\",\"to_symbol\":\"ETH\",\"change_24_hour\":0," +
+				DisplayDataRaw: "{\"from_symbol\":\"btc\",\"to_symbol\":\"usdt\",\"change_24_hour\":0," +
 					"\"changepct_24_hour\":0,\"open_24_hour\":60867.47,\"volume_24_hour\":666.1991214442407," +
 					"\"volume_24_hour_to\":40641405.87766658,\"low_24_hour\":0,\"high_24_hour\":61403.89," +
-					"\"price\":61391.27,\"supply\":30701,\"mkt_cap\":1215020604533,\"last_update\":1719731197640}",
+					"\"price\":61391.27,\"supply\":30701,\"mkt_cap\":0,\"last_update\":1719731197640}",
 			},
 		},
 		{
 			name: "empty",
-			d:    &cryptoCompareWsData{},
+			args: args{
+				d: &huobiWsData{},
+			},
 			want: &domain.Data{
 				DisplayDataRaw: "{\"from_symbol\":\"\",\"to_symbol\":\"\",\"change_24_hour\":0," +
 					"\"changepct_24_hour\":0,\"open_24_hour\":0,\"volume_24_hour\":0,\"volume_24_hour_to\":0," +
@@ -158,14 +176,16 @@ func Test_convertCryptoCompareWsDataToDomain(t *testing.T) {
 		},
 		{
 			name: "nil",
-			d:    nil,
+			args: args{
+				d: nil,
+			},
 			want: nil,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := convertCryptoCompareWsDataToDomain(tt.d); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("convertCryptoCompareWsDataToDomain() = %v, want %v", got, tt.want)
+			if got := convertHuobiWsDataToDomain(tt.args.from, tt.args.to, tt.args.d); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("convertHuobiWsDataToDomain() = %v, want %v", got, tt.want)
 			}
 		})
 	}
